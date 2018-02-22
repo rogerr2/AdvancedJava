e5eb027f0fb2e36dbeeaa6d79222fde19971e9c2
rogerr2 <35306165+rogerr2@users.noreply.github.com>
2018-02-21 22:42:40 -0600

    Homework

--- SecurityJava.java
+++ SecurityJava.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class SecurityJava {
+	
+	public static void main(String[] args) {
+		
+		Scanner oScan = new Scanner(System.in);
+		SecurityApp SecurityO = new SecurityApp();
+		//System.out.print();
+	
+	SecurityO.checker(oScan.nextLine());
+	}
 
+	
 }
+
